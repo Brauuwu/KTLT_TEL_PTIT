a6267abae258f343a3bcbc6caf243c461f92db32
Brauuwu <survivalminecraft080@gmail.com>
2024-09-12 23:22:09 +0700

    Create KTCOBAN004.cpp

---
+++ KTCOBAN004.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int k; cin>>k;
+		ll ans=9*pow(10,(k-1)/2);
+		cout<<ans<<endl;
+	}
+}
