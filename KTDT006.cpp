aab24119a678e9eb0601f09fa02b7250666a1bfe
Brauuwu <survivalminecraft080@gmail.com>
2024-10-21 01:06:21 +0700

    Hiệu 2 số lớn theo phần bù của 10

--- KTDT006.cpp
+++ KTDT006.cpp
@@ -2,22 +2,17 @@
 using namespace std;
 
 string ADD(string a, string b){
-	if(a.size()>b.size()) a.swap(b);
 	string res;
-	int n1=a.size(), n2=b.size();
-	int diff=n2-n1, carry=0;
-	for(int i=n1-1; i>=0; i--){
-		int x=(a[i]-'0')+(b[i+diff]-'0')+carry;
-		res+=char(x%10+'0');
+	int l=max(a.size(), b.size());
+	while(a.size()<l) a="0"+a;
+	while(b.size()<l) b="0"+b;
+	int carry=0;
+	for(int i=l-1;i>=0;i--){
+		int x=(a[i]-'0')+(b[i]-'0')+carry;
+		res=char(x%10+'0')+res;
 		carry=x/10;
 	}
-	for(int i=n2-n1-1; i>=0; i--){
-		int x=(b[i]-'0')+carry;
-		res+=char(x%10+'0');
-		carry=x/10;
-	}
-	if(carry) res+=char(carry+'0');
-	reverse(res.begin(), res.end());
+	if(carry) res="1"+res;
 	return res;
 }
 
@@ -31,18 +26,18 @@ string complement10(string s){
 }
 
 string SUB(string a, string b){
-	if(a.size()<b.size()) a.swap(b);
 	string res;
-	int n1=a.size(), n2=b.size();
-	int diff=n1-n2;
-	for(int i=0; i<diff; i++) b="0"+b;
+	if(a.size()<b.size()) a.swap(b);
+	int l=max(a.size(), b.size());
+	while(a.size()<l) a="0"+a;
+	while(b.size()<l) b="0"+b;
 	res=ADD(a,complement10(b));
-	if(res.size()>a.size()){
+	if(res.size()>l){
 		res.erase(0,1);
 		while(res[0]=='0') res.erase(0,1);
 		return res;
 	}
-	return complement10(res);
+	else return complement10(res);
 }
 
 int main(){
