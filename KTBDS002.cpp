88666458559c49e329d2aa0cbf5b936023f65da3
Brauuwu <survivalminecraft080@gmail.com>
2024-12-20 03:53:45 +0700

    Biểu diễn số 02

--- KTBDS002.cpp
+++ KTBDS002.cpp
@@ -1,21 +1,26 @@
 #include<bits/stdc++.h>
 #define ll long long
 #define ull unsigned long long
+#define oo 1e9
 using namespace std;
 
-ull ans(ull n,ull k){
-	ull res=1, j=1;
-	for(ull i=n+1;i<n+k;i++){
-		res*=i;
-		if(j<k) res/=j++;
+ll C[100][100];
+
+void Pascal(){
+	for(int i=0; i<=100; i++){
+		for(int j=0; j<=i; j++){
+			if(j==0 || j==i) C[i][j]=1;
+			else C[i][j] = C[i-1][j] + C[i-1][j-1];
+		}
 	}
-	return res; 
 }
 
 int main(){
+	Pascal();
 	int t; cin>>t;
-	while(t--){
-		ull n,k; cin>>n>>k;
-		cout<<ans(n,k)<<endl;
+    while(t--){
+    	int n, k; cin>>n>>k;
+    	cout<<C[n+k-1][n]<<endl;
 	}
+    return 0;
 }
